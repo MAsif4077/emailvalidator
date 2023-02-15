c75d6a2b4c47fb050b3ad151ab633d27d6f7f5d1
MAsif4077 <asifalicb2@gmail.com>
2023-02-15 11:50:39 +0500

    git ignore is added

--- controller/validateEmailController.js
+++ controller/validateEmailController.js
@@ -40,5 +40,75 @@ module.exports={
         }
   
     },
+        validateBulk: async function(req,res){
+      try{
+        const email=req.body.emails;
+    
+        var lines=email.split(/\n/);
+        console.log("lines ",lines);;
+    
+        var output=[];
+        for(var i=0;i<lines.length;i++){
+            if(/\S/.test(lines[i])){
+                output.push(lines[i].trim());
+            }
+    
+        }
+       console.log("output",output);
+       var emails=output;
+           if (!Array.isArray(emails)) {
+             res.send("Error: Email input should be an array.");
+           }  else{ var validEmails = [];
+             var invalidEmails = [];
+             var acceptAllEmails = [];
+             var pendingVerification = [];
+             var promises = [];
+             for (const email of emails) {
+               var domain = email.split('@')[1];
+           var emailAddress = `${randomstring.generate(10)}@${domain}`
+               // console.log("Domain", domain);
+               // console.log("emailAddress", emailAddress);
+               var promise = new Promise((resolve, reject) => {
+           emailVerify.verify(emailAddress,async function(err, info) {
+                   if (err) {
+                     await pendingVerification.push(email);
+                    // console.log("Pending ",pendingVerification);
+                   } else if (info.success) {
+                   await acceptAllEmails.push(email);
+                    // console.log("Accept-All",acceptAllEmails)
+                   } else {
+                 emailVerify.verify(email,async function(err, info) {
+                       if (info.success) {
+                        await validEmails.push(email);
+                       //  console.log("Valid",validEmails)
+                       } else {
+                        await invalidEmails.push(email);
+                        // console.log("Invalid ",invalidEmails)
+                       }
+                     })
+                   }
+                   resolve();
+                 })
+               });
+               promises.push(promise);
+               console.log(promise);
+             }
+             Promise.all(promises).then(() => {
+              var data={
+               valid:validEmails,
+               invalid:invalidEmails,
+               acceptAll:acceptAllEmails,
+               pending:pendingVerification
+              }
+              console.log("data....",data);
+               res.render('bulkEmails',{title:'Bulk Domain Validator',text:data,flag:true});
+             });
+           }
+    }catch(err){
+        res.send(err);
+    }
+       
+
+    }
 
     }
--- index.js
+++ index.js
@@ -6,15 +6,20 @@ const emailController=require('./controller/validateEmailController')
 
 const app = express();
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({extended: false}));
+app.get('/', (req, res)=> {
+    res.render('bulkEmails',{title:'Bulk Domain Validator',text:'',flag:false});
 
+})
 
-app.get('/', (req, res) => {
+app.get('/single', (req, res) => {
     res.render('index',{info:''});
 })
 app.post('/bulkemailchecker',emailController.validateEmail)
+app.post('/bulkEmailValidate',emailController.validateBulk);
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
